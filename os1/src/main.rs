5724d84e76f19b7294cbf6fa96ba30143cc7e296
lewis56654281 <2408859305@qq.com>
2024-11-06 11:44:13 +0800

    完成 ch1 练习

---
+++ os1/src/main.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#![no_std]
+#![feature(panic_info_message)]
+mod lang_items;
+mod sbi;
+#[macro_use]
+mod console;
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    print!("Some text");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
